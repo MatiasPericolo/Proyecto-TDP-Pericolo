25dd6125d456ae4faaaeef30f4fb40a14ab55718
MatiasPericolo <54510812+MatiasPericolo@users.noreply.github.com>
2020-09-22 18:49:06 -0300

    Intento de fallo

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 	
 	public static void main(String[] args){
-		Persona p=new Persona("Pedro");
+		Persona p=new Persona("Jorge");
 		System.out.println(" "+p.getNombre());
 	}
 }
