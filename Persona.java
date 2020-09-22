08f5055c9c52298809a28b2a1972f45107f7c4e4
MatiasPericolo <matiaspericolo@gmail.com>
2020-09-22 17:54:01 -0300

    comit inicial

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona{
+	private String nombre;
+
+	public Persona(String name){
+		nombre=name;
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void setNombre(String name){
+		nombre=name;
+	}
+}
---
+++ Prueba.java
@@ -0,0 +1,7 @@
+public class Prueba{
+	
+	public static void main(String[] args){
+		Persona p=new Persona("Matias");
+		System.out.println(" "+p.getNombre());
+	}
+}
