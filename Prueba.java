2302817710cd5cdfcfe15ddcd127ba6ba708f846
MatiasPericolo <54510812+MatiasPericolo@users.noreply.github.com>
2020-09-22 18:43:29 -0300

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 	
 	public static void main(String[] args){
-		Persona p=new Persona("Matias");
+		Persona p=new Persona("Jose");
 		System.out.println(" "+p.getNombre());
 	}
 }
