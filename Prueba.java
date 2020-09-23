f8589c9c64939a2ebee6ee753c18a421a77637ff
MatiasPericolo <matiaspericolo@gmail.com>
2020-09-23 12:24:25 -0300

    commit_error_1

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 	
 	public static void main(String[] args){
-		Persona p=new Persona("Jorge");
+		Persona p=new Persona("Mariano");
 		System.out.println(" "+p.getNombre());
 	}
 }
