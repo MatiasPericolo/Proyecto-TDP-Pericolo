192dde72cdc54705d1e773c07bf2a129ebd7f786
MatiasPericolo <matiaspericolo@gmail.com>
2020-09-23 18:21:00 -0300

    Prueba_gitignore

--- Ignorar.java
+++
@@ -1,6 +0,0 @@
-public class Ignorar{
-
-	public static void main(String[] args){
-		System.out.println("Deberia estar ignorado");
-	}
-}
--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,6 @@
 public class Prueba{
 	
 	public static void main(String[] args){
-		Persona p=new Persona("Juancho");
-		System.out.println(" "+p.getNombre());
+		System.out.println("Juancho");
 	}
 }
